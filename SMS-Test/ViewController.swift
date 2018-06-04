7944e84e1f8ce03ea2fe6bb42df80fd9e02cca91
Sam MacGinty <smacgint@nd.edu>
2018-06-04 14:43:26 -0500

    Initial Commit

---
+++ SMS-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SMS-Test
+//
+//  Created by Sam MacGinty on 04/06/2018.
+//  Copyright © 2018 Sam MacGinty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
